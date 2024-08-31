edc7c36af52e8a2211bb8c3f12b0088a15f16934
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-08-31 14:12:05 +0900

    Time: 34 ms (68.16%), Space: 16.5 MB (75.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,21 +1,25 @@
 class Solution:
+    import math
+    
     def uniquePaths(self, m: int, n: int) -> int:
         
-        grid = []
+#         grid = []
         
-        for i in range(m):
-            temp = []
-            for j in range(n):
-                if i == 0 or j == 0:
-                    path_number = 1
-                else:
-                    path_number = 0
-                temp.append(path_number)
-            grid.append(temp)
+#         for i in range(m):
+#             temp = []
+#             for j in range(n):
+#                 if i == 0 or j == 0:
+#                     path_number = 1
+#                 else:
+#                     path_number = 0
+#                 temp.append(path_number)
+#             grid.append(temp)
         
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 0:
-                    grid[i][j] = grid[i][j - 1] + grid[i - 1][j]
+#         for i in range(m):
+#             for j in range(n):
+#                 if grid[i][j] == 0:
+#                     grid[i][j] = grid[i][j - 1] + grid[i - 1][j]
 
-        return grid[m-1][n-1]
+#         return grid[m-1][n-1]
+        
+        return math.factorial(m+n-2) // (math.factorial(m-1) * math.factorial(n-1))
